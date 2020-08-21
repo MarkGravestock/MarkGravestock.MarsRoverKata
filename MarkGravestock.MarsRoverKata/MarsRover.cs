ae30d4c419e056a4e32f3c8f79c955196ec1a860
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-08-21 15:39:19 +0100

    Implemented Right move & Extract class

--- MarkGravestock.MarsRoverKata/MarsRover.cs
+++ MarkGravestock.MarsRoverKata/MarsRover.cs
@@ -3,25 +3,64 @@ namespace MarkGravestock.MarsRoverKata
     // TODO: Assuming start point is 0:0:N Input M Output
     // TODO: Input "M", Output "2:0:N" 
 
+    public class Direction
+    {
+        private readonly string[] directions = { "N", "W", "S", "E" };
+        private int directionOffset = 0;
+
+        public string GetDirection()
+        {
+            return directions[directionOffset];
+        }
+
+        public void TurnRight()
+        {
+            if (directionOffset == 0)
+            {
+                directionOffset = 3;
+            }
+            else
+            {
+                directionOffset = (directionOffset - 1) % 4;
+            }
+        }
+
+        public void TurnLeft()
+        {
+            directionOffset = (directionOffset + 1) % 4;
+        }
+    }
+
     public class MarsRover
     {
         private int x = 0;
-        private string direction = "N";
-        
-        public string Result => $"{x}:0:{direction}";
+
+        private readonly Direction direction = new Direction();
+
+        public string Result => $"{x}:0:{direction.GetDirection()}";
 
         public void Command(string commands)
         {
             foreach (var command in commands)
             {
                 if (command == 'L')
-                { 
-                    direction = "W";
-                    return;
+                {
+                    direction.TurnLeft();
                 }
-
-                x++;
+                else if (command == 'R')
+                {
+                    direction.TurnRight();
+                }
+                else
+                {
+                    MoveInDirection();
+                }
             }
+        }
+
+        private void MoveInDirection()
+        {
+            x++;
         }
     }
 }
--- MarkGravestock.MarsRoverKata/MarsRoverTest.cs
+++ MarkGravestock.MarsRoverKata/MarsRoverTest.cs
@@ -10,6 +10,13 @@ namespace MarkGravestock.MarsRoverKata
         [InlineData("MMM", "3:0:N")]
         [InlineData("MMML", "3:0:W")]
         [InlineData("L", "0:0:W")]
+        [InlineData("LL", "0:0:S")]
+        [InlineData("LLL", "0:0:E")]
+        [InlineData("LLLL", "0:0:N")]
+        [InlineData("R", "0:0:E")]
+        [InlineData("RR", "0:0:S")]
+        [InlineData("RRRR", "0:0:N")]
+
         public void when_the_rover_executes_a_command_the_output_is_correct(string command, string expectedOutput)
         {
             var sut = new MarsRover();
