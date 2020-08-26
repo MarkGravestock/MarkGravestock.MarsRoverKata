595ad4975f63d3df5846fab9d97824ed1cd4560c
MarkGravestock <MarkGravestock@users.noreply.github.com>
2020-08-26 18:04:36 +0100

    Begin Refactor to State Pattern

--- MarkGravestock.MarsRoverKata/MarsRover.cs
+++ MarkGravestock.MarsRoverKata/MarsRover.cs
@@ -1,8 +1,70 @@
+using System;
+
 namespace MarkGravestock.MarsRoverKata
 {
+    public class North : IDirection
+    {
+        public IDirection TurnLeft()
+        {
+            return new West();
+        }
+
+        public IDirection TurnRight()
+        {
+            return new East();
+        }
+    }
+
+    public class East : IDirection
+    {
+        public IDirection TurnLeft()
+        {
+            return new North();
+        }
+
+        public IDirection TurnRight()
+        {
+            return new South();
+        }
+    }
+
+    public class West : IDirection
+    {
+        public IDirection TurnLeft()
+        {
+            return new South();
+        }
+
+        public IDirection TurnRight()
+        {
+            return new North();
+        }
+    }
+
+    public class South : IDirection
+    {
+        public IDirection TurnLeft()
+        {
+            return new East();
+        }
+
+        public IDirection TurnRight()
+        {
+            return new West();
+        }
+    }
+
+    public interface IDirection
+    {
+        IDirection TurnLeft();
+        IDirection TurnRight();
+    }
+
     public class MarsRover
     {
         private readonly Grid grid;
+        
+        private IDirection _direction = new North();
         
         private readonly Direction direction = new Direction();
         
@@ -25,10 +87,12 @@ namespace MarkGravestock.MarsRoverKata
             {
                 if (command == 'L')
                 {
+                    _direction = _direction.TurnLeft();
                     direction.TurnLeft();
                 }
                 else if (command == 'R')
                 {
+                    _direction = _direction.TurnRight();
                     direction.TurnRight();
                 }
                 else if (command == 'M')
@@ -50,15 +114,15 @@ namespace MarkGravestock.MarsRoverKata
         private void MoveInDirection()
         {
             //state.Move()
-            if (direction.GetDirection() == "N")
+            if (_direction is North)
             {
                 position.MoveNorth();
             }
-            else if (direction.GetDirection() == "S")
+            else if (_direction is South)
             {
                 position.MoveSouth();
             }
-            else if (direction.GetDirection() == "W")
+            else if (_direction is West)
             {
                 position.MoveWest();
             }
